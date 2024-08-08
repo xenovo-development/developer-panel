e407a98e2290005139c4471e204c4fc91515212f
damlahocaoglu <damlahocaogluu@gmail.com>
2024-08-08 12:40:28 +0300

    update

--- resources/views/apps/guest24dev.blade.php
+++ resources/views/apps/guest24dev.blade.php
@@ -55,7 +55,7 @@
                         Below you can see an image of the test site of the Guest24 website.
                        </p>
                        <h5 class="mb-3 d-inline-block">Developer Website Link:</h5>
-                        <a href="http://www.dev.guest24.services/" target="_blank" class="text-muted d-inline-block">
+                        <a href="http://dev.guest24.services/" target="_blank" class="text-muted d-inline-block">
                         dev.guest24.services
                         </a>
                         <!-- İframe ekleyelim -->                      
@@ -63,7 +63,7 @@
                     </div>
                     <!-- end card-body-->
                 </div>
-                <iframe src="http://www.dev.guest24.services/" width="100%" height="800px" style="border: none;"></iframe>
+                <iframe src="http://dev.guest24.services/" width="100%" height="800px" style="border: none;"></iframe>
                 <!-- end card-->
             </div>
             <!-- end col-12 -->
